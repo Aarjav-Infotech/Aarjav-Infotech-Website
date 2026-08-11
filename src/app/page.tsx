7c5aeb3983b3ee9f73e6c41e442d232ff1acef4b
disha dave <dishadave409@gmail.com>
2026-08-11 20:17:26 +0530

    feat: error solved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@
 
 import { useEffect } from "react";
 import HomeContent from "@/features/home/components/home-content";
-import AboutContent from "@/features/about/components/about-content";
-import AiContent from "@/features/services/ai-workflow-page/ai-workflow-content";
+// import AboutContent from "@/features/about/components/about-content";
+// import AiContent from "@/features/services/ai-workflow-page/ai-workflow-content";
 
 export default function HomePage() {
   useEffect(() => {
@@ -27,8 +27,8 @@ export default function HomePage() {
   return (
     <main id="main-content" className="w-full">
       <HomeContent />
-      <AboutContent />
-      <AiContent />
+      {/* <AboutContent /> */}
+      {/* <AiContent /> */}
     </main>
   );
 }
