e4059d498bafd5ed9f5f71047f51a7b2aa1b0c0f
disha dave <dishadave409@gmail.com>
2026-08-11 20:13:07 +0530

    feat: add service and about pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { useEffect } from "react";
-// import HomeContent from "@/features/home/components/home-content";
-// import AboutContent from "@/features/about/components/about-content";
+import HomeContent from "@/features/home/components/home-content";
+import AboutContent from "@/features/about/components/about-content";
 import AiContent from "@/features/services/ai-workflow-page/ai-workflow-content";
 
 export default function HomePage() {
@@ -26,8 +26,8 @@ export default function HomePage() {
 
   return (
     <main id="main-content" className="w-full">
-      {/* <HomeContent />
-      <AboutContent/> */}
+      <HomeContent />
+      <AboutContent />
       <AiContent />
     </main>
   );
